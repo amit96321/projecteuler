8bffbab73c4d7de1b8b6987213fb084a01e18b96
amit96321 <kumaramit96321@gmail.com>
2019-02-06 11:57:12 +0530

    Update P15LatticePaths.cpp

--- P15LatticePaths.cpp
+++ P15LatticePaths.cpp
@@ -1,6 +1,10 @@
 /*Problem: Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
 there are exactly 6 routes to the bottom right corner. How many such routes are there through a 20×20 grid?
 Author: Amit Kumar(kumaramit96321@gmail.com)
+
+Another Way:How many ways can we allocate m R’s and n D’s in such a string? Note that for a single configuration, once we place the the R’s we immediately know where the D’s must go (as they cannot go
+anywhere else). All we really have to know is how many ways we can place the R’s. The number
+of ways we can do this, mathematically, is denoted with the binomial coefficient m+n C m
 */
 
 #include<bits/stdc++.h>
